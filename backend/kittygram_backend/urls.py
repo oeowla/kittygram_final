5af1b5d8b0bb19b726653b438782c0271b9825b2
oeowla <danil18092007@icloud.com>
2025-12-08 01:27:40 +0300

    Update urls.py

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -3,7 +3,19 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from cats.views import AchievementViewSet, CatViewSet
-EOF
 
-python -m isort --check-only --diff test_urls.py
+from cats.views import AchievementViewSet, CatViewSet
+
+router = routers.DefaultRouter()
+router.register('achievements', AchievementViewSet)
+router.register('cats', CatViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
